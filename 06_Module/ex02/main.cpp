b9045634615b7b477cc5166baf158b148c4cf1e5
Miguel Carrelo Paulino <micarrel@c2r4s2.42lisboa.com>
2024-05-14 17:13:54 +0100

    /

---
+++ 06_Module/ex02/main.cpp
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: micarrel <micarrel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/14 15:04:38 by micarrel          #+#    #+#             */
+/*   Updated: 2024/05/14 17:08:07 by micarrel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include "Base.hpp"
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+
+Base *gererate(void)
+{
+	int i = rand() % 3;
+	if (i == 0)
+		return new A;
+	else if (i == 1)
+		return new B;
+	else
+		return new C;
+}
+
+void	identify(Base* p)
+{
+	if (dynamic_cast<A*>(p))
+		std::cout << "A" << std::endl;
+	else if (dynamic_cast<B*>(p))
+		std::cout << "B" << std::endl;
+	else if (dynamic_cast<C*>(p))
+		std::cout << "C" << std::endl;
+}
+
+void	identify(Base& p)
+{
+	if (dynamic_cast<A*>(&p))
+		std::cout << "A" << std::endl;
+	else if (dynamic_cast<B*>(&p))
+		std::cout << "B" << std::endl;
+	else if (dynamic_cast<C*>(&p))
+		std::cout << "C" << std::endl;
+	else
+		std::cout << "Unknown type" << std::endl;
+}
+
+int main()
+{
+	srand(time(NULL));
+	Base *base = gererate();
+	identify(base);
+	identify(*base);
+	delete base;
+	return 0;
+}
