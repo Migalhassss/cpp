3b432bc2783e3af6ee59285eaa758b09a5a44b13
Miguel Carrelo Paulino <micarrel@c2r1s2.42lisboa.com>
2024-05-16 18:07:41 +0100

    .

--- 07_Module/ex01/iter.hpp
+++ 07_Module/ex01/iter.hpp
@@ -17,14 +17,14 @@
 
 template <typename T>
 
-void    print(T const &x)
+void	print(T const &x)
 {
 	std::cout << x << std::endl;
 }
 
 template <typename T>
 
-void    iter(T *array, int length, void (*f)(T const &))
+void	iter(T *array, int length, void (*f)(T const &))
 {
 	int i = 0;
 
---
+++ 07_Module/ex02/Array.hpp
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Array.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: micarrel <micarrel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/16 14:45:12 by micarrel          #+#    #+#             */
+/*   Updated: 2024/05/16 15:49:17 by micarrel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ARRAY_HPP
+# define ARRAY_HPP
+
+# include <iostream>
+# include <cstdlib>
+
+template <typename T>
+class Array
+{
+	private:
+		T		*_array;
+		unsigned int	_size;
+	public:
+		Array(void) : _array(NULL), _size(0) {};
+		Array(unsigned int n) : _array(new T[n]), _size(n) {};
+		Array(const Array &src) : _array(new T[src._size]), _size(src._size) {
+			*this = src;
+		}
+		~Array(void) {
+			delete [] _array;
+		}
+		class	OutOfBound : public std::exception {
+			public:
+				virtual const char *what() const throw() {
+					return "Out of bound";
+				}
+		};
+		Array &operator=(const Array &src) {
+			if (this != &src) {
+				delete [] _array;
+				_array = new T[src._size];
+				_size = src._size;
+				for (unsigned int i = 0; i < _size; i++)
+					_array[i] = src._array[i];
+			}
+			return *this;
+		}
+		T &operator[](unsigned int n) {
+			if (n >= _size || n < 0)
+				throw OutOfBound();
+			return _array[n];
+		}
+
+		unsigned int size() const 
+		{
+			return _size;
+		}
+};
+
+#endif
