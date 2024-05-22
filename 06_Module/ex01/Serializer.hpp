fbb00dec9dd4f3c7a6ad2652febfdc55be1642ce
Miguel Carrelo Paulino <micarrel@c2r1s1.42lisboa.com>
2024-05-22 14:25:54 +0100

    .

--- 06_Module/ex01/Serializer.hpp
+++ 06_Module/ex01/Serializer.hpp
@@ -6,7 +6,7 @@
 /*   By: micarrel <micarrel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/13 16:30:39 by micarrel          #+#    #+#             */
-/*   Updated: 2024/05/13 16:56:39 by micarrel         ###   ########.fr       */
+/*   Updated: 2024/05/22 14:20:12 by micarrel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,12 @@
 
 class Serializer
 {
-	public:
+	private:
 		Serializer();
 		Serializer(Serializer const & src);
-		~Serializer();
 		Serializer & operator=(Serializer const & rhs);
-
+	public:
+		~Serializer();
 		static uintptr_t serialize(Data* ptr);
 		static Data* deserialize(uintptr_t raw);
 };
--- 06_Module/ex01/main.cpp
+++ 06_Module/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: micarrel <micarrel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/13 16:53:01 by micarrel          #+#    #+#             */
-/*   Updated: 2024/05/13 17:02:04 by micarrel         ###   ########.fr       */
+/*   Updated: 2024/05/22 14:19:37 by micarrel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,11 @@ int main()
 	data.s1 = "Hello";
 	data.s2 = "World";
 
+	
 	uintptr_t raw = Serializer::serialize(&data);
+
+	std::cout << "raw: " << raw << std::endl;
+
 	Data* ptr = Serializer::deserialize(raw);
 
 	std::cout << "n: " << ptr->n << std::endl;
